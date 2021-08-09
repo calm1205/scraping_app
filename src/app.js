38a1efab57ef3e3301f08256a8f783b449ce42db
nagi-moriyama-lvgs <nagi.moriyama@leverages.jp>
2021-08-09 21:35:41 +0900

    スクレイピング機能を実装

--- src/app.js
+++ src/app.js
@@ -1,11 +1,96 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("button");
-  console.log("hello world");
-  console.log(button);
+// 次のページのURLを作成
+const nextPageURL = (document, mainURL, nextPageSelector) => {
+  if (!nextPageSelector) return false;
+  const nextPageDOM = document.querySelector(nextPageSelector);
+  const nextPageHref = nextPageDOM?.getAttribute("href");
+  if (!nextPageHref) return false;
+  const domain = mainURL.match(/^http.*:\/\/[^\/]+\//)[0];
+  const nextPageURLIsAbsolute = /^http/.test(nextPageHref);
+  if (nextPageURLIsAbsolute) {
+    return nextPageHref;
+  } else {
+    return domain + nextPageHref;
+  }
+};
 
-  button.addEventListener("click", () => {
-    alert("hello");
-    const title = document.querySelector("h1");
-    title.style.color = "red";
+// スクレイピング結果をファイルでダウンロード
+const makeOutputFile = (output) => {
+  let blob = new Blob([output], { type: "text/plan" });
+  let link = document.createElement("a");
+  link.href = URL.createObjectURL(blob);
+  link.download = "output.txt";
+  link.click();
+};
+
+// スクレイピング
+const scraping = async (output, args) => {
+  const { url, parentSelector, selectors, nextPageSelector } = args;
+  const result = await fetch(url);
+  const html = await result.text();
+  const parser = new DOMParser();
+  const virtualDocument = parser.parseFromString(html, "text/html");
+
+  Array.from(virtualDocument.querySelectorAll(parentSelector)).forEach(
+    (dom) => {
+      selectors.forEach((selector, index) => {
+        output += dom.querySelector(selector).textContent;
+        if (selectors.length != index + 1) {
+          output += ",";
+        }
+      });
+      output += "\r\n";
+    }
+  );
+
+  const nextURL = nextPageURL(virtualDocument, url, nextPageSelector);
+  if (nextURL) {
+    args.url = nextURL;
+    scraping(output, args);
+  } else {
+    makeOutputFile(output);
+    const scrapingButton = document.getElementById("scraping");
+    scrapingButton.textContent = "Start Scraping";
+    scrapingButton.disabled = false;
+    return false;
+  }
+};
+
+// +ボタンでフォームを追加
+const addNewInput = (box, button) => {
+  const index = button.getAttribute("data-id");
+  const newInput = `<div class="input_form">
+                      <p>Child Selector: ${index}</p>
+                      <input type="text" class="selector" />
+                    </div>`;
+  box.insertAdjacentHTML("beforebegin", newInput);
+  button.setAttribute("data-id", Number(index) + 1);
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  // +ボタンでフォームを追加
+  const addSelectorBox = document.getElementById("add_selector");
+  const addSelectorButton = document.getElementById("add");
+  addSelectorButton.addEventListener("click", () =>
+    addNewInput(addSelectorBox, addSelectorButton)
+  );
+
+  // Scrapingボタンでスクレイピングの開始
+  const scrapingButton = document.getElementById("scraping");
+  scrapingButton.addEventListener("click", () => {
+    const output = "";
+    const selectorsDOM = Array.from(document.querySelectorAll(".selector"));
+    const selectorsValue = [];
+    selectorsDOM.forEach((dom) => {
+      if (dom.value) selectorsValue.push(dom.value);
+    });
+    const args = {
+      url: document.getElementById("url").value,
+      parentSelector: document.getElementById("parent_selector").value,
+      selectors: selectorsValue,
+      nextPageSelector: document.getElementById("next_page").value,
+    };
+    scrapingButton.textContent = "Now Scraping ...";
+    scrapingButton.disabled = true;
+    scraping(output, args);
   });
 });
---
+++ src/readme.md
@@ -0,0 +1,21 @@
+# サンプルのスクレイピング対象
+
+## url
+
+https://job.rikunabi.com/2022/search/company/result/?fw=&ms=0&kk=0&wk=2&wk=3&wk=4&wk=5&aot=&apr=&cwr=&cmr=&rry=3&rr=&ggr=1&gr=&awy=&wer=&wmr=&exr=1&hu=1
+
+## Parent Selector
+
+.ts-h-search-cassette
+
+## Chile Selector1
+
+.ts-h-search-cassetteTitleMain
+
+## Chile Selector2
+
+.ts-h-search-cassetteRemainingTextCounter
+
+## next page selector
+
+.ts-h-search-pipePagerLink
